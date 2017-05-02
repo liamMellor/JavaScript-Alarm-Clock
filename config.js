2c70c21e8c07e9dd8ed58125b661aeee363b0c66
liamMellor <liam.r.mellor@gmail.com>
2017-05-01 23:25:13 -0400

    fixed bugs with config and index.html.

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 System.config({
     // Map "index" to the "build" folder
     map: {
-      index: 'build' 
+      main: 'build'
     },
     // Define settings for loading files in "build"
     packages: {
       build: {
-        index: 'index.js',
+        main: 'index.js',
         format: 'system',
         defaultExtension: 'js'
       }
